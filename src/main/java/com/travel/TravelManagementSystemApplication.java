3712bb6510e8268a0b1c383b3dcf0367cd967141
Sairaj Rajankar <sairajrajankar508@gmail.com>
2026-05-29 20:34:15 +0530

    Refactor: Update dependencies and clean up commented code in application files

--- src/main/java/com/travel/TravelManagementSystemApplication.java
+++ src/main/java/com/travel/TravelManagementSystemApplication.java
@@ -1,18 +1,3 @@
-//package com.travel;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class TravelManagementSystemApplication {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(TravelManagementSystemApplication.class, args);
-//	}
-//
-//}
-
-
 package com.travel;
 
 import org.springframework.boot.SpringApplication;
